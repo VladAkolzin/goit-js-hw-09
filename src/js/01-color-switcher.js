8c25dc475e8fa63f1e164f6ab8224062c868e3e1
VladAkolzin <akolzin95@gmail.com>
2023-11-18 11:51:08 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+refs.start.addEventListener('click', () => {
+  refs.start.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 2000);
+});
+refs.stop.addEventListener('click', () => {
+  refs.start.removeAttribute('disabled');
+  clearInterval(timerId);
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
