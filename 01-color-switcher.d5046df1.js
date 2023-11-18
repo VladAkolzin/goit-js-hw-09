e26669930f8fab2c02bed036b1550dcdb57b4c3f
VladAkolzin <138156026+VladAkolzin@users.noreply.github.com>
2023-11-18 10:58:11 +0000

    Deploying to gh-pages from @ VladAkolzin/goit-js-hw-09@e590df587fd9ebdac29cacf735c2c4ceccab1ba3 🚀

--- 01-color-switcher.d5046df1.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};t.start.addEventListener("click",(()=>{t.start.setAttribute("disabled",!0),timerId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),2e3)})),t.stop.addEventListener("click",(()=>{t.start.removeAttribute("disabled"),clearInterval(timerId)}));
-//# sourceMappingURL=01-color-switcher.d5046df1.js.map
